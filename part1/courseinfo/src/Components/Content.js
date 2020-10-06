82415e25f8fa3c5ef44d2b0fe2759d339a703a6a
Alex Hamilton <alex.b.hamilton56@gmail.com>
2020-10-06 19:43:17 -0400

    1.3 - Refactored for use of object constants

--- part1/courseinfo/src/Components/Content.js
+++ part1/courseinfo/src/Components/Content.js
@@ -6,15 +6,12 @@ const Content = (props) => {
     <>
       <Part
         part={props.part1}
-        exercise={props.exercise1}
       />
       <Part
         part={props.part2}
-        exercise={props.exercise2}
       />
       <Part
         part={props.part3}
-        exercise={props.exercise3}
       />
     </>
   );
