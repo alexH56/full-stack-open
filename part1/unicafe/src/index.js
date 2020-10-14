231499261c942827abc53cf7fea35786afbf2da0
Alex Hamilton <alex.b.hamilton56@gmail.com>
2020-10-13 21:51:03 -0400

    Fixed merge error

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,10 +1,7 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import Feedback from './components/Feedback';
 import Stats from './components/Stats';
-=======
->>>>>>> master
 
 const App = () => {
   // save clicks of each button to own state
@@ -13,16 +10,10 @@ const App = () => {
   const [bad, setBad] = useState(0);
 
   return (
-<<<<<<< HEAD
     <>
       <Feedback />
       <Stats />
     </>
-=======
-    <div>
-      code here
-    </div>
->>>>>>> master
   );
 };
 
