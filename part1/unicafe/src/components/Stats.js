403ac56e010273d5b74c9dfc8e6333a86d5be83a
Alex Hamilton <alex.b.hamilton56@gmail.com>
2020-10-19 22:17:53 -0400

    1.7 -  Added additional statistics

--- part1/unicafe/src/components/Stats.js
+++ part1/unicafe/src/components/Stats.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 const Stats = ({ good, bad, neutral }) => {
+  const all = good + bad + neutral;
+  const average = good + (bad * -1) + (neutral * 0);
+  const percent = good > 0 ? good / all * 100 : 0;
+
   return (
     <>
       <h1>Statistics</h1>
@@ -8,6 +12,9 @@ const Stats = ({ good, bad, neutral }) => {
         <li>{`Good: ${good}`}</li>
         <li>{`Neutral: ${neutral}`}</li>
         <li>{`Bad: ${bad}`}</li>
+        <li>{`All: ${all}`}</li>
+        <li>{`Average: ${average}`}</li>
+        <li>{`Positive: ${percent}%`}</li>
       </ul>
     </>
   );
