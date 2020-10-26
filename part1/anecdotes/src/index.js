a51a47bd34d1d3e58a78bea1f387de035e756526
Alex Hamilton <alex.b.hamilton56@gmail.com>
2020-10-25 20:13:24 -0400

    1.13 - Added voting feature

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,25 +1,6 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = (props) => {
-  const [selected, setSelected] = useState(0);
-
-  const handleClick = () => () => {
-    setSelected(Math.floor(Math.random() * anecdotes.length));
-  };
-
-  return (
-    <>
-      <div>
-        {props.anecdotes[selected]}
-      </div>
-      <button onClick={handleClick()}>
-        next anecdote
-      </button>
-    </>
-  );
-};
-
 const anecdotes = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
@@ -28,6 +9,39 @@ const anecdotes = [
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ];
+
+const App = ({ anecdotes }) => {
+  const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(Array.apply(null, new Array(anecdotes.length)).map(Number.prototype.valueOf, 0));
+
+  const handleClick = () => () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length));
+  };
+
+  const handleVote = () => () => {
+    const pointsCopy = [...points];
+    pointsCopy[selected] += 1;
+    setPoints(pointsCopy);
+  };
+
+  return (
+    <>
+      <div>
+        {`${anecdotes[selected]}`}
+      </div>
+      <div>
+        {`has ${points[selected]} votes`}
+      </div>
+      <button onClick={handleClick()}>
+        next anecdote
+      </button>
+      <button onClick={handleVote()}>
+        vote
+      </button>
+
+    </>
+  );
+};
 
 ReactDOM.render(
   <App anecdotes={anecdotes} />,
